6f0bd48956ff32f2d5ca6f468014c065238cf567
castmoon <academicgmag@gmail.com>
2021-05-31 14:21:08 -0300

    test: ensure ListUsersCOntroller returns all users

--- src/tests/controllers/ListUsersController.spec.js
+++ src/tests/controllers/ListUsersController.spec.js
@@ -4,11 +4,31 @@ const readTestDatabase = require('../database/testDatabaseLoader')
 
 
 describe('ListUsersController', () => {
+
+    const makeListUserController = () => {
+        return new ListUsersController([{
+            "id": "test_id",
+            "email": "test@test.com",
+            "password": "test"
+        }]);
+    }
+
     test('should throws if there no users in db', () => {
-        const listUsersController = new ListUsersController(readTestDatabase());
+        const listUsersController = makeListUserController();
+        listUsersController.database = [];
         const listUsers = listUsersController.listAllUsers();
         expect(listUsers.body).toEqual(new NotFoundError('users'));
         expect(listUsers.statusCode).toBe(400);
     });
     
+    test('should return users', () => {
+        const listUsersController = makeListUserController();
+        const listAllUsers = listUsersController.listAllUsers();
+        expect(listAllUsers.body).toEqual([{
+            "id": "test_id",
+            "email": "test@test.com",
+            "password": "test"
+        }]);
+        expect(listAllUsers.statusCode).toBe(200);
+    });
 });
